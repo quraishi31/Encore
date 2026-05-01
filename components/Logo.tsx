22eac1b1449c5582c43bc4d63600c36973a70848
quraishi31 <muhammadamir.port@gmail.com>
2026-05-01 17:25:37 +0500

    Fix logo image source case sensitivity

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export function Logo({ className = 'h-14 w-auto' }: { className?: string }) {
   return (
     <Image
-      src="/Encore.png"
+      src="/encore.png"
       alt="Encore"
       width={200}
       height={56}
