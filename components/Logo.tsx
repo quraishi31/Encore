6b3d42cf49cff9fdc96b06c14c7879b051548f91
quraishi31 <muhammadamir.port@gmail.com>
2026-05-02 01:38:29 +0500

    Update aria-label in Logo component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -9,7 +9,7 @@ export function Logo({ className = 'h-14 w-auto' }: { className?: string }) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
-      aria-label="11th hour"
+      aria-label="11th Hour"
       role="img"
     >
       <defs>
